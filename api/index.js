7ec32167d8b8149c90c6b29c239cca7cebbc7e61
Jaqueline Kirino <bfatskih@gmail.com>
2022-02-07 15:59:18 -0300

    port 8080

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 const fs = require('fs');
 
 // static resources should just be served as they are
