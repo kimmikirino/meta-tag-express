42706455292bc97dd9a0ee7ce673fb54e211a48e
Jaqueline Kirino <bfatskih@gmail.com>
2022-02-07 14:15:24 -0300

    first commit

---
+++ api/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 3001;
+const fs = require('fs');
+
+// static resources should just be served as they are
+app.use(express.static(path.resolve(__dirname, '..', 'public'), { maxAge: '30d' }));
+
+app.listen(PORT, error => {
+  if (error) {
+    return console.log('Error during app startup', error);
+  }
+  console.log('listening on ' + PORT + '...');
+});
+
+const indexPath = path.resolve(__dirname, '..', 'public', 'index.html');
+app.get('/*', (req, res, next) => {
+  fs.readFile(indexPath, 'utf8', (err, htmlData) => {
+    if (err) {
+      console.error('Error during file reading', err);
+      return res.status(404).end();
+    }
+    // get post info
+    const postId = req.query.id;
+    console.log(postId);
+    // const post = getPostById(postId);
+    // if(!post) return res.status(404).send("Post not found");
+
+    // inject meta tags
+    htmlData = htmlData
+      .replace('<title>Omella</title>', `<title>aaaa</title>`)
+      .replace('Omella: Payments, forms and signatures, all in one place', 'JAQUELINE');
+    // .replace('__META_OG_DESCRIPTION__', post.description)
+    // .replace('__META_DESCRIPTION__', post.description)
+    // .replace('__META_OG_IMAGE__', post.thumbnail)
+
+    console.log(htmlData);
+    return res.send(htmlData);
+  });
+});
